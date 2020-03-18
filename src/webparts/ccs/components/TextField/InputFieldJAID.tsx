8b8874e8c7a872a83564d917f09de90be072e54c
Ted Lalakas <lalasted@gmail.com>
2020-03-18 16:53:54 +1100

    Added some logic to the JAID for entering too many numbers. Just for testing porposes

--- src/webparts/ccs/components/TextField/InputFieldJAID.tsx
+++ src/webparts/ccs/components/TextField/InputFieldJAID.tsx
@@ -13,6 +13,10 @@ const InputFieldJAID = (props) => {
         onChange={(ev, newValue) => props.changeHandler(newValue)}
         // styles={{ fieldGroup: { width: 300 } }}
         value={inputValue}
+        // errorMessage="Error message"
+        onGetErrorMessage={(value) => {
+          return inputValue.length <= 6 ? '' : `Length must be no more than 6. Actual length is ${inputValue.length}.`;
+        }}
       />
     </React.Fragment>
   );
